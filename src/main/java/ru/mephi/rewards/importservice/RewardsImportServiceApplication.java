a71097b8a6b1fd5de720f54e8e34a117f11d66a3
artemyyu <wrestlingteam57@gmail.com>
2025-11-06 00:15:16 +0300

    init commit

---
+++ src/main/java/ru/mephi/rewards/importservice/RewardsImportServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mephi.rewards.importservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsImportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsImportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mephi/rewards/importservice/RewardsImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mephi.rewards.importservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsImportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
